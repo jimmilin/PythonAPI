5db0094af57ece3187af2e1e23513ce8ee86c4e7
jimmilin <jimmilin0531@gmail.com>
2017-03-26 17:44:49 -0700

    comment and optimize code

--- main.py
+++ main.py
@@ -11,75 +11,65 @@ import requests
 import json
 import csv
 
+
+## API key and uid for accessing form information
 key = "ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d"
 uid = "PfSNFM"
-data_header = list();
-# url = "https://api.typeform.com/v1/form/PfSNFM?key=ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d"
 
+## list for future use
+
+##x-axis header
+data_header = list()
+##question id for identifying response
+question_id = list()
+
+
+## url for request
 url = "https://api.typeform.com/v1/form/" + uid + "?key=" + key
 
+
+##sending request for response
 resp = requests.get(url)
 data = resp.json()
 
 
 
 
-
-          
+##check if connection is fine          
 if data['http_status'] == 200:
+    ##open file for writing
     write_data = open('.\data.csv', 'w', encoding='UTF-8')
-    data_writer = csv.writer(write_data)
-    
+    data_writer = csv.writer(write_data, lineterminator='\n')
+
+    ##filling header information
     for quest in data['questions']:
         data_header.append(quest['question'])
-
+        question_id.append(quest['id'])
+        
     data_writer.writerow(data_header)
-##    print(data['responses'][0]['completed'])
 
+    ##filling response information
     for response in data['responses']:
+        
         data_answers = list()
+        
+        ##only if form was completed
         if response['completed'] != '0':
-            if 'date_46098446' in response['answers']:
-                data_answers.append(response['answers']['date_46098446'])
-            else:
-                data_answers.append('NaN')
-            if 'number_46099137' in response['answers']:
-                data_answers.append(response['answers']['number_46099137'])
-            else:
-                data_answers.append('NaN')
-            if 'yesno_46099317' in response['answers']:
-                data_answers.append(response['answers']['yesno_46099317'])
-            else:
-                data_answers.append('NaN')
-            if 'list_46100308_choice' in response['answers']:
-                data_answers.append(response['answers']['list_46100308_choice'])
-            else:
-                data_answers.append('NaN')
-            if 'textarea_46100513' in response['answers']:
-                data_answers.append(response['answers']['textarea_46100513'])
-            else:
-                data_answers.append('NaN')
+
+            ##check if 
+            for i in range(0, len(question_id)):
+                if question_id[i] in response['answers']:
+                    data_answers.append(response['answers'][question_id[i]])
+                else:
+                    data_answers.append('NaN')
+           
             data_writer.writerow(data_answers)
-        
-##        for answer in response['answers']:
-##            print(answer)
-            
-##            if answer['yesno_46099317'] == '' or answer['yesno_46099317'] is None:
-##                data_answers.append('NaN')
-##            else:
-##                data_answers.append(answer['yesno_46099317'])
-##        data_writer.writerow(data_answers)
-
+       
         
 
-
+##close it down, boys. It is done
 write_data.close()
 
 
 
 
-
-
-##f = open('E:\document\GitHub\PythonAPI\data.csv', 'w', encoding='UTF-8', newline='')
-##f.write(rcomp.text)
-##f.close()
